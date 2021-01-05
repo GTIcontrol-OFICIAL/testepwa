0ae0feca8a017ee00a73bd0901b8ab83c7339301
GTIcontrol <46675802+GTIcontrol-OFICIAL@users.noreply.github.com>
2021-01-05 14:41:42 -0300

    Troquei de lugar

--- sw.js
+++ sw.js
@@ -23,3 +23,21 @@ self.addEventListener('fetch', function(e) {
     })
   );
 });
+
+
+//forçar instalar------------------------
+ buttonInstall.addEventListener('click', (e) => {
+        // Hide the app provided install promotion
+        hideMyInstallPromotion();
+        // Show the install prompt
+        deferredPrompt.prompt();
+        // Wait for the user to respond to the prompt
+        deferredPrompt.userChoice.then((choiceResult) => {
+          if (choiceResult.outcome === 'accepted') {
+            console.log('User accepted the install prompt');
+          } else {
+            console.log('User dismissed the install prompt');
+          }
+        });
+      });
+  //forçar instalar------------------------
